db447734052207da73886179ea74899e2c7c7929
Sai Siddharth Tadepalli <9sidharth96@mail.com>
2018-05-13 01:59:23 +0530

    Changed Server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const dbmLabURL = require("./config/db.js").urlMlab;
 
 const app = express();
 const port = 8000 || process.env.PORT;
-
+const dbURI = "mongodb://heroku_g94zw77l:f0d9iv95ffp87vuvcq43ssd9f1@ds221990.mlab.com:21990/heroku_g94zw77l";
 {
   /*
       Use this line to get the passed json data from postman 
@@ -35,7 +35,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // });
 
 //For mLab connection
-MongoClient.connect(dbmLabURL, function(err, db) {
+// MongoClient.connect(dbmLabURL, function(err, db) {
+//   if (err) return console.log("Error connecting to DB " + err);
+//   console.log("Connected to Database");
+  
+//   notePostRoute(app, db);
+
+//   app.listen(port, function() {
+//     console.log("Server started on port " + port);
+//   });
+// });
+//Test for heroku
+MongoClient.connect(process.env.MONGODB_URI || dbURI, function(err, db) {
   if (err) return console.log("Error connecting to DB " + err);
   console.log("Connected to Database");
   
